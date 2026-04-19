9073ec78af38320369174b1cd6391ffd362b1211
Rodrigo Sandoval Musito <rodrigo.sandoval.musito@gmail.com>
2026-04-19 10:18:29 -0600

    Fix: Agregar limpieza al after_install

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd /var/www/html
+
+# 1. Apagar y limpiar cualquier contenedor o red vieja que esté estorbando
+# (El '|| true' evita que el script marque error si no encuentra nada que borrar)
+sudo /usr/local/bin/docker-compose down || true
+
+# 2. Construir y levantar la versión nueva
 sudo /usr/local/bin/docker-compose up -d --build
