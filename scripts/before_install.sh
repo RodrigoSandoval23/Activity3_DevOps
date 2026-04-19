61b0e7a4412c453830bc7ea688bc25c9e1507493
Rodrigo Sandoval Musito <rodrigo.sandoval.musito@gmail.com>
2026-04-19 10:30:06 -0600

    Fix: Anclar Docker Compose a v2.25.0 para compatibilidad

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -6,6 +6,6 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
-# Instalar Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# Instalar una versión anclada y estable de Docker Compose (v2.25.0)
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
